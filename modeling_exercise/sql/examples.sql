1886ac6d850046b9c3ae2e760ec6b0fd1a753dae
hamzaabouhanifa <habouhanifa@tf1.fr>
2022-08-01 05:12:50 +0200

    sql example update

--- modeling_exercise/sql/examples.sql
+++ modeling_exercise/sql/examples.sql
@@ -5,8 +5,13 @@ WHERE T.is_published = 1
 AND T.date_published BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 MONTH) AND CURDATE();
 
 -- What country had the highest number of publications last month?
-SELECT c.country, count(t.trip_id) as number_of_publications FROM mydb.City c
+WITH tmp as(
+	SELECT t.trip_id, t.origin FROM mydb.Trip t
+	WHERE t.is_published = 1
+	AND t.date_published BETWEEN DATE_SUB(CURDATE(), INTERVAL 1 MONTH) AND CURDATE()
+	)
+SELECT c.country, count(tmp.trip_id) as number_of_publications FROM mydb.City c
 INNER JOIN mydb.Stops s ON s.city_id = c.city_id
-INNER JOIN mydb.Trip t ON t.origin = s.stop_id
+INNER JOIN tmp ON tmp.origin = s.stop_id
 GROUP BY c.country
 ORDER BY number_of_publications DESC LIMIT 1;
